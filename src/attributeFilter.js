5d0af8feb3e763012ef2d8d8667dd6896342c8cf
Víctor González <victor.gonzalez@geomati.co>
2017-10-25 09:57:50 +0200

    Add autocompletion for attribute filter

--- src/attributeFilter.js
+++ src/attributeFilter.js
@@ -1,4 +1,6 @@
 import './attributeFilter.css';
+import Awesomplete from 'awesomplete';
+import 'awesomplete/awesomplete.css';
 
 function json(url) {
   return new Promise(function (resolve) {
@@ -12,10 +14,13 @@ function json(url) {
   });
 }
 
-function createUI(fields, container) {
+function createUI(fields, parent) {
+  let container = document.createElement('div');
+  container.classList = ['attribute-filter-container'];
+  parent.append(container);
+
   let select = document.createElement('select');
   select.classList = ['attribute-filter-field'];
-  container.append(select);
 
   fields.forEach(field => {
     let option = document.createElement('option');
@@ -25,38 +30,82 @@ function createUI(fields, container) {
 
   let input = document.createElement('input');
   input.type = 'text';
-  input.classList = ['attribute-filter'];
+  input.classList = ['attribute-filter', 'dropdown-input'];
+
+  container.append(select);
   container.append(input);
 
-  return { input, select };
+  let awesomplete = new Awesomplete(input);
+  return { select, awesomplete };
 }
 
 export function gl(opts, map, layers) {
   let layer = layers.filter(l => opts.layer === l.id)[0];
+  let awesomplete;
+  let input;
+  let select;
+  let fieldValues = {};
 
-  function search(field, text) {
-    if (field && text) {
-      layer.setParams({ cql_filter: `${field} ILIKE '%${text}%'` });
+  function wfs(request, query) {
+    let url = `${layer._url}?service=wfs&version=1.1.0&request=${request}&typeName=${layer.options.layers}&outputFormat=application/json`;
+    if (query) url += `&${query}`;
+    return json(url);
+  }
+
+  function search() {
+    if (select.value && input.value) {
+      layer.setParams({ cql_filter: `${select.value} ILIKE '%${input.value}%'` });
     } else {
       delete layer.wmsParams.cql_filter;
       layer.setParams({});
     }
   }
 
-  let url = `${layer._url}?service=wfs&version=1.1.0&request=DescribeFeatureType&typeName=${layer.options.layers}&outputFormat=application/json`;
-  json(url).then(function (data) {
+  function updateAutocomplete() {
+    let field = select.value;
+    if (!field) return;
+    if (fieldValues[field]) {
+      awesomplete.list = fieldValues[field];
+    } else {
+      awesomplete.list = [];
+      wfs('GetFeature', `propertyName=${field}`).then(function (data) {
+        if (!data.features) return;
+        fieldValues[field] = data.features
+          .map(f => f.properties[field])
+          .filter(function (item, pos, self) {
+            return item && self.indexOf(item) === pos;
+          });
+        awesomplete.list = data.features
+          .map(f => f.properties[field])
+          .filter(function (item, pos, self) {
+            return item && self.indexOf(item) === pos;
+          }); // fieldValues[field];
+      });
+    }
+  }
+
+  wfs('DescribeFeatureType').then(function (data) {
     let fields = data.featureTypes[0].properties.map(p => p.name);
     if (opts && opts.excludeFields) {
       fields = fields.filter(f => !opts.excludeFields.includes(f));
     }
-    let { input, select } = createUI(fields, map.getContainer());
 
-    select.addEventListener('click', e => e.stopPropagation());
-    input.addEventListener('click', e => e.stopPropagation());
+    ({ select, awesomplete } = createUI(fields, map.getContainer()));
+    input = awesomplete.input;
+
+    [input.parentNode, select, input].forEach(c => {
+      c.addEventListener('click', e => e.stopPropagation());
+      c.addEventListener('dblclick', e => e.stopPropagation());
+    });
     input.addEventListener('mouseover', () => map.dragging.disable());
     input.addEventListener('mouseout', () => map.dragging.enable());
+
     input.addEventListener('keypress', function (e) {
-      if (e.charCode === 13) search(select.value, input.value);
+      if (e.charCode === 13) search();
     });
+    input.addEventListener('awesomplete-selectcomplete', search);
+    select.addEventListener('change', updateAutocomplete);
+
+    updateAutocomplete();
   });
 }
