30ef7704e4399b018c70cfe58fc13ed23ef3ec14
Víctor González <victor.gonzalez@geomati.co>
2017-10-25 17:11:19 +0200

    Add bounds option to map

--- src/map.js
+++ src/map.js
@@ -2,5 +2,9 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export function gl(opts) {
-  return L.map(opts.id, opts.vendorOptions);
+  let map = L.map(opts.id, opts.vendorOptions);
+
+  if (opts.bounds) map.fitBounds(opts.bounds);
+
+  return map;
 }
