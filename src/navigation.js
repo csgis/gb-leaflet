a6fe7bd64d203ba0fa32461ac45f3c1b89bb57a3
Víctor González <victor.gonzalez@geomati.co>
2017-10-26 12:49:21 +0200

    Fix navigation control to use map center/zoom after init

--- src/navigation.js
+++ src/navigation.js
@@ -3,5 +3,8 @@ import 'leaflet-navbar/src/Leaflet.NavBar';
 import 'leaflet-navbar/src/Leaflet.NavBar.css';
 
 export function gl(opts, map) {
-  L.control.navbar().addTo(map);
+  L.control.navbar({
+    center: map.getCenter(),
+    zoom: map.getZoom()
+  }).addTo(map);
 }
