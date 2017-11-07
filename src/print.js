5ee800aca5da6ef2ef58010a514d25f7f10585d6
Víctor González <victor.gonzalez@geomati.co>
2017-11-07 10:50:02 +0100

    Improve print module to filter layers by url regex

--- src/print.js
+++ src/print.js
@@ -1,15 +1,26 @@
 import L from 'leaflet';
 import 'jquery';
-import 'leaflet-print';
+// import 'leaflet-print';
+// import 'leaflet-print/dist/leaflet.print.css';
 import 'leaflet-print/dist/leaflet.print.css';
+import 'leaflet-print/src/print.Provider';
+import 'leaflet-print/src/Control.Print';
+
+const DEFAULT_PROVIDER_OPTS = {
+  autoLoad: true,
+  dpi: 300,
+  url: '/geoserver/pdf',
+  outputFilename: 'map'
+};
 
 export function gl(opts, map) {
-  let printProvider = L.print.provider({
-    method: 'GET',
-    url: opts.url,
-    autoLoad: true,
-    dpi: 90
-  });
+  let printProvider = L.print.provider(Object.assign({}, DEFAULT_PROVIDER_OPTS, opts.provider));
+  let f = printProvider._getLayers.bind(printProvider);
+  let rs = opts.urlRegexes;
+  let matches = layer => !rs || rs.some(r => layer._url.match(r));
+  printProvider._getLayers = function (x) {
+    return f(x).filter(l => !l.isBaseLayer && matches(l));
+  };
 
   let printControl = L.control.print({
     provider: printProvider
