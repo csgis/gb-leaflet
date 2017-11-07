a85681989e3e340d0e0700530a74bbfc58de99c4
Víctor González <victor.gonzalez@geomati.co>
2017-11-07 10:45:30 +0100

    Remove console.log

--- src/time-control.js
+++ src/time-control.js
@@ -4,7 +4,6 @@ import L from 'leaflet';
 export function gl(opts, map) {
   let added = false;
   map.timeDimension.on('availabletimeschanged', e => {
-    console.log(e.availableTimes);
     if (!added && e.availableTimes.length) {
       L.control.timeDimension().addTo(map);
       added = true;
