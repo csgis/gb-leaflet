e050e9436fa525e31c898dad023195e65878584b
Víctor González <victor.gonzalez@geomati.co>
2018-01-22 11:23:02 +0100

    Change fullscreen syntax to ES5

--- src/fullscreen.js
+++ src/fullscreen.js
@@ -2,4 +2,6 @@ import L from 'leaflet';
 import 'leaflet-fullscreen';
 import 'leaflet-fullscreen/dist/leaflet.fullscreen.css';
 
-export let gl = (opts, map) => L.control.fullscreen().addTo(map);
+export function bricjs(opts, map) {
+  return L.control.fullscreen().addTo(map);
+}
