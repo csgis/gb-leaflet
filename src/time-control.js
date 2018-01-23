57b1c6a3a8e85d70eb3eda6313b80522b3d77a78
Víctor González <victor.gonzalez@geomati.co>
2018-01-23 11:28:06 +0100

    Add possibility to disable time filter

--- src/time-control.js
+++ src/time-control.js
@@ -4,10 +4,39 @@ import L from 'leaflet';
 
 export function bricjs(opts, map) {
   let added = false;
+
   map.timeDimension.on('availabletimeschanged', e => {
     if (!added && e.availableTimes.length) {
-      L.control.timeDimension(opts).addTo(map);
       added = true;
+
+      let control = L.control.timeDimension(opts);
+      map.addControl(control);
+
+      let getCurrentTime = map.timeDimension.getCurrentTime;
+
+      let container = control._container;
+      let controls = Array.from(container.childNodes)
+        .filter(elem => !elem.classList.contains('disabled'));
+      controls.push(container);
+
+      let button = document.createElement('a');
+      button.classList.add('leaflet-control-timecontrol');
+      button.classList.add('time-control-power');
+      button.addEventListener('click', function () {
+        if (button.classList.contains('disabled')) {
+          map.timeDimension.getCurrentTime = getCurrentTime;
+          map.timeDimension.setCurrentTime(Date.now());
+          controls.map(elem => (elem.style.visibility = 'visible'));
+        } else {
+          map.timeDimension.getCurrentTime = () => null;
+          map.timeDimension.fire('timeload', 0);
+          controls.map(elem => (elem.style.visibility = 'hidden'));
+        }
+
+        button.classList.toggle('disabled');
+      });
+
+      container.appendChild(button);
     }
   });
 }
